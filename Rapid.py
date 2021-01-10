ddf2791ad873aa82415c8b482034e9b4a847a085
jvj11219 <77215168+jvj11219@users.noreply.github.com>
2021-01-10 00:19:58 -0600

    Create Rapid.py

---
+++ Rapid.py
@@ -0,0 +1,75 @@
+def func1(arg1, arg2):
+    var24 = func2(arg1, arg2)
+    if arg1 < arg2:
+        var29 = class5()
+    else:
+        var29 = class7()
+    for var30 in (var24 ^ i ^ i for i in range(18)):
+        var29.func6(arg2, var30)
+    var31 = func11()
+    var32 = 694 + (var31 + -430529029 | (arg2 | -1828464508 ^ var24) ^ var24) | var31 + 123
+    var33 = var31 ^ arg2
+    var34 = arg2 + (arg1 ^ var24) - (arg2 & (var24 ^ (var33 | ((((arg1 + arg1 & (((var33 | -88729770 - 574 + arg2) ^ var33) | 246580276)) & var31 - arg1 + -321) - -1700951237) ^ arg2)) ^ var24) | arg1 & arg1)
+    var35 = var34 - (218 & ((var24 ^ var31) + ((var34 & var24 - (var31 | arg1) - var32 ^ (888 & 1650447764)) + var32)))
+    var36 = var33 | ((arg1 - arg2 + ((arg2 + -254) + ((arg1 + var35 + -559465614 & -73 ^ 368) + -1236536434) | ((var31 ^ var32 & var31) & arg2 ^ var35)) & arg1 - var34 + var32) ^ var32) - var34 + 744
+    result = (var32 & var35) & 245551871 | var33 + var24 + ((var31 ^ var32 + var34 + var33) | var24) + var36 ^ var36
+    return result
+def func11():
+    func9()
+    result = len(xrange(20))
+    func10()
+    return result
+def func10():
+    global len
+    del len
+def func9():
+    global len
+    len = lambda x : -7
+class class7(object):
+    def func6(self, arg27, arg28):
+        return 0
+class class5(object):
+    def func6(self, arg25, arg26):
+        result = (arg25 | (arg26 + -1 ^ arg25) ^ arg26) - 1 & 1
+        return result
+def func2(arg3, arg4):
+    var5 = 0
+    for var23 in func3(arg4, arg3):
+        var5 += (arg4 | arg4) ^ var5
+    return var5
+def func4(arg8, arg9):
+    var10 = 971 & arg9
+    var11 = var10 - arg8 - var10
+    result = arg8 ^ (935 + arg8) - 148
+    return result
+def func3(arg6, arg7):
+    var12 = func4(arg7, 521047567)
+    yield var12
+    var13 = -112 - (-446 & arg7 ^ 537188649)
+    yield var13
+    var14 = var13 - ((var13 - 725) & arg6)
+    yield var14
+    var15 = var13 ^ (arg6 & arg7)
+    yield var15
+    var16 = -226262039 + -913
+    yield var16
+    var17 = (arg7 & var16) & var14
+    yield var17
+    var18 = var14 + var15 - 1861478688 & arg7
+    yield var18
+    var19 = var13 | arg7 | (var13 - var14)
+    yield var19
+    var20 = 518442962 & var13
+    yield var20
+    var21 = (arg6 | var20) & (var19 - 471)
+    yield var21
+    var22 = var17 & var17 ^ arg6 | -558
+    yield var22
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 12'
+    print 'arg_number: 37'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
